de22d6e87c6cf25d77e8173135ca5299faf1d3dc
David Weinstein <dweinst@insitusec.com>
2026-01-07 21:36:37 -0500

    fix: allow docs to build

--- config/dev.exs
+++ config/dev.exs
@@ -1,12 +1,8 @@
 import Config
 
 # Dev config - reads DATABASE_URL from environment
-database_url =
-  System.get_env("DATABASE_URL") ||
-    raise """
-    environment variable DATABASE_URL is missing.
-    Example: postgresql://user:password@localhost:5432/database_name
-    """
-
-config :db_sampler,
-  database_url: database_url
+# Optional to allow building docs without a database
+if database_url = System.get_env("DATABASE_URL") do
+  config :db_sampler,
+    database_url: database_url
+end
